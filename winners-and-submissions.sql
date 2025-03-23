8f9ea7ded79275736234da413413a01eab4fd8dd
daniprieto <dani101val@gmail.com>
2025-03-23 17:21:48 -0400

    Remove obsolete hackathon-specific query and add team participation query with leader and members details

--- winners-and-submissions.sql
+++ winners-and-submissions.sql
@@ -8,7 +8,10 @@ SELECT
     w.Position,
     (SELECT CONCAT(u.FirstName, ' ', u.LastName)
      FROM Users u
-     WHERE u.UserID = t.LeaderID) AS TeamLeader
+     WHERE u.UserID = t.LeaderID) AS TeamLeader,
+	(SELECT AVG(e.Score)
+	 FROM Evaluations e
+	 WHERE e.SubmissionID = w.SubmissionID) AS Score
 FROM Winners w
 JOIN Teams t ON w.TeamID = t.TeamID
 JOIN Submissions s ON w.SubmissionID = s.SubmissionID
